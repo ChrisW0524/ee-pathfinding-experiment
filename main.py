4efdbeca88be56a999f56afc20875650de104167
ChrisW0524 <chriswang20050524@gmail.com>
2022-01-20 19:46:14 +0800

    Modified data collection

--- main.py
+++ main.py
@@ -32,9 +32,11 @@ ORANGE = (255, 165 ,0) # Start node
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208) #End node    
 
-ROWS = 25
-DRAW_ANIMATION = True
+row_size = 10
 is_maze = False
+
+DRAW_ANIMATION = False
+RANDOM_DENSITY = 0.25
 
 #node class
 class Node:
@@ -137,8 +139,7 @@ class Node:
         
     def __str__(self):
         pos = self.get_pos()
-        return str(pos)
-        
+        return str(pos)       
 def make_grid(grid_size, window_size):
     grid = []
     gap = window_size // grid_size
@@ -148,16 +149,19 @@ def make_grid(grid_size, window_size):
             node = Node(i, j, gap, grid_size)
             grid[i].append(node)
             
-    return grid
-
+    start = grid[0][0]
+    start.make_start()
+    end = grid[row_size - 1][row_size - 1]
+    end.make_end()
+            
+    return (grid, start, end)
 def draw_grid_lines(window, grid_size, window_size):
     gap = window_size // grid_size
     for i in range(grid_size):
         pygame.draw.line(window, GREY, (0, i * gap), (window_size, i * gap))
         for j in range(grid_size):
             pygame.draw.line(window, GREY, (j * gap, 0), (j * gap, window_size))
-    pass
-    
+    pass   
 def draw(window, grid, grid_size, window_size):
     window.fill(WHITE)
     
@@ -166,9 +170,7 @@ def draw(window, grid, grid_size, window_size):
             spot.draw(window)
             
     draw_grid_lines(window, grid_size, window_size)
-    pygame.display.update()
-    
-    
+    pygame.display.update()   
 def get_clicked_pos(pos, rows, width):
 	gap = width // rows
 	x, y = pos
@@ -177,13 +179,11 @@ def get_clicked_pos(pos, rows, width):
 	col = x // gap
 
 	return col, row
-
 def reset_grid_colors(grid, start, end):
     for row in grid:
         for node in row:
             if(node != start and node != end and not node.is_barrier()):
-                node.reset()
-                
+                node.reset()                
 def run_algorithm(grid, start, end, algorithm):
     reset_grid_colors(grid,start,end)
     path_length = None
@@ -191,47 +191,113 @@ def run_algorithm(grid, start, end, algorithm):
         t_start = perf_counter()    
         
         if algorithm == "a_star":             
-            path_length = a_star(grid, start, end, lambda: draw(window, grid, ROWS, WINDOW_SIZE)) if DRAW_ANIMATION else a_star(grid, start, end, lambda: None)
+            path_length = a_star(grid, start, end, lambda: draw(window, grid, row_size, WINDOW_SIZE)) if DRAW_ANIMATION else a_star(grid, start, end, lambda: None)
         if algorithm == "greedy_bfs":
-            path_length = greedy_bfs(grid, start, end, lambda: draw(window, grid, ROWS, WINDOW_SIZE)) if DRAW_ANIMATION else greedy_bfs(grid, start, end, lambda: None)
+            path_length = greedy_bfs(grid, start, end, lambda: draw(window, grid, row_size, WINDOW_SIZE)) if DRAW_ANIMATION else greedy_bfs(grid, start, end, lambda: None)
         if algorithm == "bi_bfs":
-            path_length = bi_bfs(grid, start, end, lambda: draw(window, grid, ROWS, WINDOW_SIZE)) if DRAW_ANIMATION else bi_bfs(grid, start, end, lambda: None)
+            path_length = bi_bfs(grid, start, end, lambda: draw(window, grid, row_size, WINDOW_SIZE)) if DRAW_ANIMATION else bi_bfs(grid, start, end, lambda: None)
         
         if(path_length is None):
             if(is_maze):
-                generate_maze(grid, ROWS, start, end)
+                generate_maze(grid, row_size, start, end)
             else:
-                randomize_grid(grid, 0.25, start, end)
+                randomize_grid(grid, RANDOM_DENSITY, start, end)
                 
         t_stop = perf_counter()
     
     return (t_stop - t_start, path_length)
-
+def collect_data():
+    global row_size
+    MIN_GRID_SIZE = 10
+    MAX_GRID_SIZE = 50
+    GRID_ITERATIONS = 10
+    ALGORITHM_ITERATIONS = 10
+    
+    #Random environment
+    f = open("random_grid.csv", "w+", newline="")
+    f.truncate()
+    writer = csv.writer(f)
+    column_header = ("Grid size","A* Average time", "A* Path Length", "Greedy BFS Average time", "Greedy BFS Path Length", "Bi-BFS Average time", "Bi-BFS Path Length")
+    writer.writerow(column_header)
+        
+    #Loop through different grid sizes
+    for grid_size in range(MIN_GRID_SIZE, MAX_GRID_SIZE):
+        row_size = grid_size
+        grid, start, end = make_grid(row_size, WINDOW_SIZE)
+        
+        #loop through each grid size for some iterations
+        for i in range(GRID_ITERATIONS):
+            randomize_grid(grid, RANDOM_DENSITY, start, end)
+            path_length_data = [0, 0, 0]
+            time_data = [[],[],[]]
+            
+            #For each grid run each algorithm for x times and calculate their average time
+            for j in range(ALGORITHM_ITERATIONS):
+                result = run_algorithm(grid, start, end, "a_star")
+                time_data[0].append(result[0]) 
+                path_length_data[0] = result[1] 
+                
+                result = run_algorithm(grid, start, end, "greedy_bfs")
+                time_data[1].append(result[0])  
+                path_length_data[1] = result[1]
+                
+                result = run_algorithm(grid, start, end, "bi_bfs")
+                time_data[2].append(result[0])  
+                path_length_data[2] = result[1]
+                
+            writer.writerow((grid_size, path_length_data[0], sum(time_data[0])/len(time_data[0]), path_length_data[1], sum(time_data[1])/len(time_data[1]), path_length_data[2], sum(time_data[2])/len(time_data[2])))
+    f.close()
+    
+    #Generated mazes
+    f = open("generated_maze.csv", "w+", newline="")
+    f.truncate()
+    writer = csv.writer(f)
+    column_header = ("Grid size","A* Average time", "A* Path Length", "Greedy BFS Average time", "Greedy BFS Path Length", "Bi-BFS Average time", "Bi-BFS Path Length")
+    writer.writerow(column_header)
+    
+    # Loop through different grid sizes
+    for grid_size in range(MIN_GRID_SIZE, MAX_GRID_SIZE):
+        row_size = grid_size
+        grid, start, end = make_grid(row_size, WINDOW_SIZE)
+        
+        #loop through each grid size for some iterations
+        for i in range(GRID_ITERATIONS):
+            generate_maze(grid, row_size, start, end)
+            path_length_data = [0, 0, 0]
+            time_data = [[],[],[]]
+            
+            #For each grid run each algorithm for x times and calculate their average time
+            for j in range(ALGORITHM_ITERATIONS):
+                result = run_algorithm(grid, start, end, "a_star")
+                time_data[0].append(result[0]) 
+                path_length_data[0] = result[1] 
+                
+                result = run_algorithm(grid, start, end, "greedy_bfs")
+                time_data[1].append(result[0])  
+                path_length_data[1] = result[1]
+                
+                result = run_algorithm(grid, start, end, "bi_bfs")
+                time_data[2].append(result[0])  
+                path_length_data[2] = result[1]
+                
+            writer.writerow((grid_size, path_length_data[0], sum(time_data[0])/len(time_data[0]), path_length_data[1], sum(time_data[1])/len(time_data[1]), path_length_data[2], sum(time_data[2])/len(time_data[2])))
+    f.close()
+    
 def main(window, window_size):
-    grid = make_grid(ROWS, window_size)
-    
-    start = grid[0][0]
-    start.make_start()
-    end = grid[ROWS - 1][ROWS - 1]
-    end.make_end()
-    
+    global row_size
+    grid, start, end = make_grid(row_size, window_size)
     run = True
     started = False
     
     while run:
-        draw(window, grid, ROWS, window_size)
+        draw(window, grid, row_size, window_size)
         for event in pygame.event.get():
-            # if pygame.mouse.get_pressed()[0]:
-            #     pos = pygame.mouse.get_pos()
-            #     col, row = get_clicked_pos(pos, ROWS, WINDOW_SIZE)
-            #     node = grid[col][row]
-            #     print(node.get_pos())
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    randomize_grid(grid, 0.25, start, end)
+                    randomize_grid(grid, RANDOM_DENSITY, start, end)
                     is_maze = False
                 if event.key == pygame.K_z:
-                    generate_maze(grid, ROWS, start, end)
+                    generate_maze(grid, row_size, start, end)
                     is_maze = True
                 if event.key == pygame.K_x:
                     result = run_algorithm(grid, start, end, "a_star")
@@ -247,6 +313,15 @@ def main(window, window_size):
                     result = run_algorithm(grid, start, end, "bi_bfs")
                     print("Time elapsed: " + str(result[0]))
                     print("Path length: " + str(result[1]))
+                    
+                if event.key == pygame.K_1:
+                    collect_data()
+                    grid, start, end = make_grid(row_size, window_size)
+                    
+                if event.key == pygame.K_2:
+                    row_size+=1
+                    grid, start, end = make_grid(row_size, window_size)
+
 
             if event.type == pygame.QUIT:
                 run = False
